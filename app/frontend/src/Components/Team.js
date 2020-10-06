74a392b72ef82f9b743177ad3064233f6ea2f03f
Andrew <andrew@admins-MacBook-Air-2.local>
2020-10-06 19:49:11 -0400

    Info about me

---
+++ app/frontend/src/Components/Team.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Team = () => {
+    return (
+        <>
+            <p className='about'>
+                Here at Vinyasa.ai, we believe in constantly striving to be a better person than you were yesterday.
+                This is why we have created a system that can allow you to track your progress using cutting
+                edge AI technology. 
+            </p>
+        </>
+    );
+};
+
+export default Team;
