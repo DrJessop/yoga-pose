573cd0aa638b20c39264199fefbf21954f84a1ec
Andrew <andrew@admins-MacBook-Air-2.local>
2020-10-06 19:48:24 -0400

    REDIS worker that processes jobs asynchronously

---
+++ app/backend/worker.py
@@ -0,0 +1,19 @@
+import os
+
+import redis
+from loguru import logger
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+redis_url = 'redis://localhost:6379' # os.getenv('REDIS_URL')
+logger.info('REDIS URL {}'.format(redis_url))
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+  logger.info('__main__')
+  with Connection(conn):
+    worker = Worker(map(Queue, listen))
+    logger.info(f'worker: {worker}')
+    worker.work()
