9d6e4c0e3f59b8c092dc459d2b12222581480fc4
Andrew <andrew@admins-MacBook-Air-2.local>
2020-10-06 10:01:58 -0400

    Receives form data for instructor and student

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,15 +1,29 @@
 from flask import (
     Flask, redirect, request, url_for
 )
-# import loguru
+from flask_cors import CORS
+from loguru import logger
+
+from rq import Queue
+from rq.job import Job
+# q = Queue(connection=conn)
 
 app = Flask(__name__)
+CORS(app)  # Cross-origin resource sharing (React app using different port than Flask app)
 
-@app.route('/register_request')
-def register():
-    registration_data = request.form
-    email = registration_data['email']
-    password = registration_data['password']
-    confirm_password = registration_data['confirm_password']
-    print(email, password, confirm_password)
-    return
+@app.route('/upload_video', methods=['POST'])
+def upload_video():
+    files = request.files
+    instructor = files['instructor']
+    student = files['student']
+
+    logger.info(instructor)
+    logger.info(student)
+
+    return {
+        'status': 200,
+        'mimetype': 'application/json'
+    }
+
+if __name__ == '__main__':
+    app.run()
