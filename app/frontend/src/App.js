6362b5af83a4059c6e6ee25153d61c49b8619b97
Andrew <andrew@admins-MacBook-Air-2.local>
2020-10-24 13:14:25 -0400

    Added more styles and passed on props to child components

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -11,40 +11,58 @@ import './App.css';
 
 const ids = ['team', 'videos', 'upload'];
 
-function active_color(menu_id) {
-    ids.forEach(id => document.getElementById(id).style.color = 'black');
-    document.getElementById(menu_id).style.color = 'green';
-}
-
-function onLoadColor() {
-    if (window.location.href.includes('meetTheTeam')) {
-        document.getElementById('team').style.color = 'green';
-    }
-    else if (window.location.href.includes('processed_videos')) {
-        document.getElementById('videos').style.color = 'green';
-    }
-    else if (window.location.href.includes('upload')) {
-        document.getElementById('upload').style.color = 'green';
-    }
-}
-
 class App extends Component {
 
+    active_color(menu_id) {
+        ids.forEach(id => document.getElementById(id).style.color = 'black');
+        document.getElementById(menu_id).style.color = 'green';
+    }
+    
+    onLoadColor() {
+        if (window.location.href.includes('meetTheTeam')) {
+            document.getElementById('team').style.color = 'green';
+        }
+        else if (window.location.href.includes('processed_videos')) {
+            document.getElementById('videos').style.color = 'green';
+        }
+        else if (window.location.href.includes('upload')) {
+            document.getElementById('upload').style.color = 'green';
+        }
+    }
+
+    updateCardsFromTitle(filePath) {
+        var fileSplit = filePath.split('/');
+        var fileName = fileSplit[fileSplit.length - 1];
+        this.updateCards(fileName, '', 'Oct 12, 2020', filePath);
+    }
+    
+    onLoadCards() {
+        fetch('http://127.0.0.1:5000/get_overlaps')
+          .then(response => response.json())
+          .then(response => JSON.parse(response.files).forEach(file => this.updateCardsFromTitle(file)));
+    }
+    
+    onLoad() {
+        this.onLoadColor();
+        this.onLoadCards();
+    }
+
     componentDidMount() {
-        window.addEventListener('load', onLoadColor);
+        window.addEventListener('load', this.onLoad());
     }
 
     constructor() {
         super();
-        this.state = {title: ["Hello"], link: ["TestLink"], date: ["October 9th, 2020"]};
-        this.ref = this;
+        this.state = {title: [], link: [], date: [], path: []};
     }
 
-    updateCards(title, link, date) {
+    updateCards(title, link, date, path) {
         var newtitle = this.state.title.concat(title);
         var newlink  = this.state.link.concat(link);
         var newdate  = this.state.date.concat(date);
-        this.setState({title: newtitle, link: newlink, date: newdate});
+        var newpath  = this.state.path.concat(path)
+        this.setState({title: newtitle, link: newlink, date: newdate, path: newpath});
+        console.log(this.state);
     }
 
     render() {
@@ -52,22 +70,22 @@ class App extends Component {
             <Router>
                 <ul id='home-menu' className='home-menu'>
                     <li className='left'>
-                        <Link id='logo' to='/' onClick={() => active_color('logo')}>
+                        <Link id='logo' to='/' onClick={() => this.active_color('logo')}>
                             <img src={wld} className='wld'/>
                         </Link>
                     </li>
                     <li className='right'>
-                        <Link to='/meetTheTeam' id='team' className='right-text' onClick={() => active_color('team')}>
+                        <Link to='/meetTheTeam' id='team' className='right-text' onClick={() => this.active_color('team')}>
                             Meet the team
                         </Link>
                     </li>
                     <li className='right'>
-                        <Link to='/processed_videos' id='videos' className='right-text' onClick={() => active_color('videos')} >
+                        <Link to='/processed_videos' id='videos' className='right-text' onClick={() => this.active_color('videos')} >
                             Your Videos
                         </Link>
                     </li>
                     <li className='right'>
-                        <Link to='/upload' id='upload' className='right-text' onClick={() => active_color('upload')}>
+                        <Link to='/upload' id='upload' className='right-text' onClick={() => this.active_color('upload')}>
                             Upload
                         </Link>
                     </li>
@@ -76,8 +94,10 @@ class App extends Component {
             <Route exact path='/meetTheTeam' component={Team} />
             <Route exact path='/processed_videos' component={() => <ProcessedVideos title={this.state.title} 
                                                                                     link={this.state.link}
-                                                                                    date={this.state.date}/>} />
-            <Route exact path='/upload' component={() => <UploadVid updateCards={(title, link, date) => this.updateCards(title, link, date)} />}/>
+                                                                                    date={this.state.date}
+                                                                                    path={this.state.path}/>} />
+            <Route exact path='/upload' component={() => <UploadVid updateCards={(title, link, date, path) => 
+                                                                                    this.updateCards(title, link, date, path)} />}/>
             </Router>
         );
     }
